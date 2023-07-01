7cc6d5fd226b7c1f6ad7d244bd81bdc3b18dfa2c
quanlin.li <quanlin.li@7x-networks.com>
2023-07-01 23:10:43 +0800

    xpilot

---
+++ build/CMakeFiles/xpilot.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/xpilot.dir/src/main.cpp.o"
+  "CMakeFiles/xpilot.dir/src/utils/file_utils.cpp.o"
+  "CMakeFiles/xpilot.dir/src/utils/string_utils.cpp.o"
+  "CMakeFiles/xpilot.dir/src/utils/int_utils.cpp.o"
+  "CMakeFiles/xpilot.dir/src/utils/command.cpp.o"
+  "xpilot.pdb"
+  "xpilot"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/xpilot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
